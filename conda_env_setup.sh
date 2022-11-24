cd5163dd07bb3aa9fa01661430e71a6ca538c906
Reinaldo G. P. Neto <reinaldo.neto@ufv.br>
2022-11-24 02:13:28 -0300

    Add files via upload

---
+++ conda_env_setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+ENV_NAME="env-proj1-393-py38"
+
+CONDA_PACKS=(
+  scikit-learn==1.1.3
+  scikit-image==0.19.3
+  matplotlib==3.6.0
+  pandas==1.5.1
+  seaborn==0.12.1
+  notebook==6.5.1
+)
+
+VERDE='\e[1;92m'
+AMARELO='\e[1;93m'
+SEM_COR='\e[0m'
+
+# ----------------------------------------------------------------------------- #
+
+conda create -yq -n $ENV_NAME python=3.8
+
+for package in ${CONDA_PACKS[@]}; do
+  conda install -yq -n $ENV_NAME -c conda-forge $package
+done
+
+echo -e "${VERDE}[INFO] - Ambiente conda configurado, para utilizar, use ${AMARELO}'conda activate $ENV_NAME'${VERDE}.${SEM_COR}"
